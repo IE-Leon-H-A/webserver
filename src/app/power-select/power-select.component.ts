565cb9e7920de88320ec54cf7be4da9f4bd42324
ibagaric <ivan.bagaric@intis.hr>
2023-06-01 19:46:57 +0200

    power select

--- src/app/power-select/power-select.component.ts
+++ src/app/power-select/power-select.component.ts
@@ -6,6 +6,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PowerSelectComponent implements OnInit {
 
+  powers = [
+    {
+      power: 50,
+      price: 0.80
+    },
+    {
+      power: 100,
+      price: 0.82
+    },
+    {
+      power: 150,
+      price: 0.85
+    },
+    {
+      power: 200,
+      price: 0.90
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
