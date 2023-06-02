ffc814d25d3cd947042c2160905ac77998c46f2d
ibagaric <ivan.bagaric@intis.hr>
2023-06-02 18:59:00 +0200

    charging html and ts

--- src/app/charging/charging.component.ts
+++ src/app/charging/charging.component.ts
@@ -1,4 +1,3 @@
-import { NONE_TYPE } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
@@ -9,81 +8,78 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./charging.component.scss']
 })
 export class ChargingComponent implements OnInit {
+  
+  // Exit flag
+  charging_active_flag = 1;
+
+  // Are changed based on user input from pevious page
+  price = 1;
+  price_limit = 1;
+  charging_power = 200;
+
+  // Hadcoded
+  start_time = new Date().getTime() / 1000;;
+  ev_capacity = 50;
+  ev_start_soc = 46;
+
+  // func vars
+  elapsed_time = 0;
+  soc = 0;
+  trasnfered_kwh = 0;
+  money_spent = 0;
+  money_left = 0;
+  time_remaining = 0;
+
+  approx_energy = this.price_limit / this.price;
+  // console.log("approx energy: " + String(approx_energy) + " kWh");
+
+  approx_time = (this.approx_energy / this.charging_power) * 3600;
+  // console.log("approx time: " + String(approx_time) + " sec");
+
 
   constructor(private dialogRef: MatDialog) {
-
     this.dialogRef.closeAll();
   }
 
   ngOnInit(): void {
-    this.mockCharging();
+    this.myLoop()
   }
 
-  mockCharging(){
-    // Exit flag
-    var charging_active_flag = 1;
+  myLoop() {
 
-    // Are changed based on user input from pevious page
-    var price = 1;
-    var price_limit = 1;
-    var charging_power = 200;
+    setTimeout(() => {
 
-    // Hadcoded
-    var start_time = new Date().getTime() / 1000;;
-    var ev_capacity = 50;
-    var ev_start_soc = 46;
+      this.elapsed_time = new Date().getTime() / 1000 - this.start_time;
+      // console.log("elapsed time: " + elapsed_time);
 
-    // func vars
-    var elapsed_time = 0;
-    var trasnfered_kwh = 0;
-    var soc = 0;
-    var money_spent = 0;
-    var money_left = 0;
-    var time_remaining = 0;
+      this.trasnfered_kwh = (this.charging_power * this.elapsed_time) / 3600;
+      // console.log("transfered energy kwh: " + trasnfered_kwh);
 
-    var approx_energy = price_limit / price;
-    // console.log("approx energy: " + String(approx_energy) + " kWh");
+      this.soc = (this.trasnfered_kwh / this.ev_capacity) * 100 + this.ev_start_soc;
+      // console.log("soc: " + soc);
 
-    var approx_time = (approx_energy / charging_power) * 3600;
-    // console.log("approx time: " + String(approx_time) + " sec");
+      this.money_spent = this.trasnfered_kwh / this.price;
+      // console.log("money spent: " + money_spent);
 
-    function myLoop() {
+      this.money_left = this.price_limit - this.money_spent;
+      // console.log("money spent: " + money_left);
 
-      setTimeout(function() {
+      if (this.money_spent > (this.price_limit - 0.05)) {
+        console.log("cost limit reached");
+        this.charging_active_flag = 0;
+        // Redirect
+      }
 
-        elapsed_time = new Date().getTime() / 1000 - start_time;
-        // console.log("elapsed time: " + elapsed_time);
+      this.time_remaining = this.approx_time - this.elapsed_time
+      console.log("time remaining: " + this.time_remaining);
 
-        trasnfered_kwh = (charging_power * elapsed_time) / 3600;
-        // console.log("transfered energy kwh: " + trasnfered_kwh);
-
-        soc = (trasnfered_kwh / ev_capacity) * 100 + ev_start_soc;
-        // console.log("soc: " + soc);
-
-        money_spent = trasnfered_kwh / price;
-        // console.log("money spent: " + money_spent);
-
-        money_left = price_limit - money_spent;
-        // console.log("money spent: " + money_left);
-
-        if (money_spent > (price_limit - 0.05)) {
-          console.log("cost limit reached");
-          charging_active_flag = 0;
-          // Redirect
-        }
-
-        time_remaining = approx_time - elapsed_time
-        console.log("time remaining: " + time_remaining);
-
-        if (charging_active_flag === 1) {
-          myLoop();
-        }
-      }, 250)
-    }
-    
-    myLoop();  
-
+      if (this.charging_active_flag === 1) {
+        this.myLoop();
+      }
+    }, 250)
   }
+
+
 
 
 }
